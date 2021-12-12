7f9e2fef1e36883eedcc41594e9954d8cc998ab8
unknown <vexterq@gmail.com>
2021-12-12 22:16:44 +0100

    Created json-server and methods for fetch single object

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,17 @@
 import { NavigationView } from "../views/NavigationView/NavigationView";
-import { fetchCharacters} from "./officeApi";
+import { fetchCharacters, fetchQuotes, fetchEpisodes} from "./officeApi";
 
 export const App = ({renderOn}) => {
     
     const urlOffice = 'https://officeapi.dev/api/characters/random';
-    const urlStarWars = 'https://swapi.dev/api/people/1';
     const navigationData = {
         link1: "LINK1",
         link2: "LINK2",
         link3: "LINK3"
     }
-    fetchCharacters(urlOffice);
-    fetchCharacters(urlStarWars);
+    fetchCharacters();
+    fetchQuotes();
+    fetchEpisodes();
 
     NavigationView({renderOn: renderOn, data: navigationData});
 
