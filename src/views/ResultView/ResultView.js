03c2a427cacad25c35277d6e65774d4166edbdcf
chrobaczyskooo@gmail.com <52071482+przemollo@users.noreply.github.com>
2021-12-14 22:05:13 +0100

    -Michael says sentences

--- src/views/ResultView/ResultView.js
+++ src/views/ResultView/ResultView.js
@@ -19,9 +19,12 @@ const templateHtml = ({ data }) => {
                 <span class="result-span id="result-questions"">${data.questions}</span> 
                 questions.
                 </p>
-                <p class="result-quote" id="result-quote"><p>
             </div>
+            
         </div>
+        <p class="result-quote">Michael says: 
+        <span id="result-quote"></span>
+        <p>
         <div class="result-inputWrp">
         <label for="result-nameId" class="result-label">Enter Your name:
         <input id=result-nameId" type="text" class="result-name" placeholder="Your name...">
@@ -39,17 +42,14 @@ export const ResultView = ({ renderOn, data }) => {
     document.querySelector(renderOn).appendChild(element);
     console.log(renderOn);
 
-    const resultQuotesArray = ["I am running away from my responsibilities. And it feels good.” – Michael Scott, Season 4, “Money”", 
-    "“I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted.” – Kevin Malone, Season 3, “Beach Games”",
-    "“And I knew exactly what to do. But in a much more real sense, I had no idea what to do.” – Michael Scott, Season 5, “Stress Relief”",
-    "“The worst thing about prison was the dementors.” – Michael “Prison Mike” Scott, Season 3, “The Convict”", 
-    "“If I don’t have some cake soon, I might die.” – Stanley Hudson, Season 4, “Survivor Man”", 
-    "“Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.” – Michael Scott, Season 2, “The Fight”",
-    "“Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.” – Michael Scott, Season 5, “The Duel”",
-    "“Oh my God it’s happening! Everybody stay calm. Stay f*cking calm!” – Michael Scott, Season 5, “Stress Relief: Part. 1”",
-    "“If I had a gun with two bullets and I was in a room with Hitler, Bin Laden and Toby, I would shoot Toby twice.” – Michael Scott, Season 6, “The Chump”"
+    const resultArrayPerfect = ["Now that’s what I call a finejob.", "Couldn’t have done it better myself.", 
+                                "That was first class work.", "You must have been practicing.", 
+                                "Good remembering!", "Marvelous!"];
+    const resultArrayMid  = ["You managed somehow.", "If I were you, I wouldn't be happy.","A result like a high school student.",
+                               "Maybe check the office again.", "I have an idea for you. Play again." ] ;             
+    const resultArrayBad = ["It was amateurish.", "Try harder.", "Did you really watch The Office?", 
+                            "That was easy to predict.", "Really?", "It was a failure." ]
 
-];
     
     const escapeBtn = document.getElementById('result-escBtn').addEventListener('click', function () {
         element.style.display = "none";
@@ -70,15 +70,26 @@ export const ResultView = ({ renderOn, data }) => {
             console.log(totalTime);
             
             if (totalTime <= 0) {
+                let result = 29;
                 const resultTime = document.getElementById('result-time');
                 resultTime.innerHTML = "minutes " + minutes + ", secundes " + secundes +".";
-                // const resultPoints = document.getElementById('result-points');
-                // const resultQuestions = document.getElementById('result-questions');
-                // const resultQuotes = document.getElementById('result-quotes');
-                // const resultQuotesChange = resultQuotes.innerHTML;
-                // for(let i = 0; i<resultQuotesArray.length; i++) {
-
-                // }
+                const resultPoints = document.getElementById('result-points');
+                const resultQuestions = document.getElementById('result-questions');
+                
+                const resultQuotes = document.getElementById('result-quote');
+                  
+                if(result<30) {
+                    let random = Math.floor(Math.random() * resultArrayBad.length);
+                    resultQuotes.innerHTML = resultArrayBad[random]; 
+                } else if (result>=30) {
+                    let random = Math.floor(Math.random() * resultArrayMid.length)
+                    resultQuotes.innerHTML = resultArrayMid[random];
+                } else if (result>70) {
+                    let random = Math.floor(Math.random() * resultArrayPerfect.length)
+                    resultQuotes.innerHTML = resultArrayPerfect[random];
+                }
+            
+                 
                 setTimeout(() => { element.style.display = "block" }, 1000);
                 clearInterval(countTimeInterval);
             }
