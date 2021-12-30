c059c468a3ceb090c013bd12ad98c5e1b2fe21db
Mateusz <mateusz.wyborski@coderscrew.pl>
2021-12-30 14:30:14 +0100

    added clock to navigation view, updated buttons, need to add logic to clock by Przemek

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,32 @@
 import { NavigationView } from "../views/NavigationView/NavigationView";
+import { TimerView } from "../views/TimerView/TimerView.js";
+import { ResultView } from "../views/ResultView/ResultView";
 
 export const App = ({renderOn}) => {
+    
 
     const navigationData = {
-        link1: "LINK1",
-        link2: "LINK2",
-        link3: "LINK3"
+
     }
 
     NavigationView({renderOn: renderOn, data: navigationData});
 
+    const clockData = {
+        buttonStart: "START",
+        buttonEnd: '🦄',
+        displayMin: "00",
+        displaySec:"00",
+        paragraphMin: "minutes",
+        paragraphSec: "secundes",
+        plusBtn: "+",
+        minBtn: "-",
+    }
+
+    // TimerView({renderOn: renderOn, data: clockData});
+
+    // const resultData = {
+       
+    // }
+
+    ResultView({renderOn: renderOn, data: resultData});
 }
--- src/views/NavigationView/NavigationView.js
+++ src/views/NavigationView/NavigationView.js
@@ -1,4 +1,5 @@
 import style from "./navigationView.styles.css";
+//import style from "./timerView.styles.css";
 import { elementFrom } from "../../shared/dom";
 
 const templateHtml = ({data}) => {
@@ -12,9 +13,9 @@ const templateHtml = ({data}) => {
                     </div>
                     <div class="office-main-body">
                         <div class="office-gamemode-bar">
-                            <p class="office-bar-mode">Character</p>
-                            <p class="office-bar-mode">Quote</p>
-                            <p class="office-bar-mode">Additional</p>
+                            <button class="office-bar-mode">Character</button>
+                            <button class="office-bar-mode">Quote</button>
+                            <button class="office-bar-mode">Additional</button>
                         </div>
                         <div class="office-gamemode-body">
                             <div class="office-gamemode-content">
@@ -23,7 +24,7 @@ const templateHtml = ({data}) => {
                                         <p>Can you guess what Character it is?</p>
                                     </div>
                                     <div class="office-gamemode-body-text">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
                                     </div>
                                 </div>
                                 <div class="office-gamemode-character-template">
@@ -32,11 +33,33 @@ const templateHtml = ({data}) => {
                                 </div>
                             </div>
                             <div class="office-gamemode-buttons">
-                                <div class="office-gamemode-playbutton">
-                                    <p>Play</p>
-                                </div>
                                 <div class="office-gamemode-halloffame">
-                                    <p>Hall of fame</p>
+                                    <button>Hall of fame</button>
+                                </div>
+                                <div class="office-gamemode-playbutton">
+                                    <button class="clock-start" id="start">Play</button>
+                                </div>
+                                <div>
+                                    <div class="clock">
+                                        <button class="clock-end" title="restart time">${data.buttonEnd}</button>
+                                    <div class="clock-display">
+                                        <div class="clock-displayWrap">
+                                            <div class="clock-displayMin" id="clock-displayMin">${data.displayMin}</div>
+                                            <div class="clock-displaySec" id="clock-displaySec">${data.displaySec}</div>
+                                        </div>
+                                        <div class="clock-displayWrap">
+                                            <p class="clock-paragraphMin clock-paragraph">${data.paragraphMin}</p>
+                                            <p class="clock-paragraphSec clock-paragraph">${data.paragraphSec}</p>
+                                        </div>
+                                    </div>
+                                    <div class="clock-btnWrap">
+                                        <button class="clock-btn clock-addMin">${data.plusBtn}</button>
+                                        <button class="clock-btn clock-remMin">${data.minBtn}</button>
+                                        <button class="clock-btn clock-addSec">${data.plusBtn}</button>
+                                        <button class="clock-btn clock-remSec" >${data.minBtn}</button>
+                                    </div>
+                                        <div class="clock-bckg"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
