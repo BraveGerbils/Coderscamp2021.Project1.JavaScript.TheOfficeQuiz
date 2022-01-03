f23569d4019ba89f525ec83bec73f66eaa7edc85
vexterq <95069336+vexterq@users.noreply.github.com>
2022-01-03 20:58:48 +0100

    Revert " Created json-server and methods for fetch single object"

--- src/app/officeApi.js
+++ src/app/officeApi.js
@@ -1,22 +1,4 @@
-import { URL } from "../shared/config";
-
-export const fetchCharacters = async () => {
-    const url = `${URL}characters`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
-
-export const fetchQuotes = async () => {
-    const url = `${URL}quotes`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
-
-
-export const fetchEpisodes = async () => {
-    const url = `${URL}episodes` ;
+export const fetchCharacters = async (url)=> {
     fetch(url)
         .then(response => response.json())
         .then(data => console.log(data));
